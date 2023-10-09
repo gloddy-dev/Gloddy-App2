43dd7dda1dd24c1af9999ac7e63d04c672799fb7
Kuesung Park <gueit214@naver.com>
2023-10-09 12:21:05 +0900

    feat : 당겼을 때 refresh

--- App.js
+++ App.js
@@ -372,6 +372,7 @@ window.addEventListener('message', function(event) {
                 setLoading(false);
               }
             }}
+            pullToRefreshEnabled
             // sharedCookiesEnabled={true}
             // scalesPageToFit={false}
             thirdPartyCookiesEnabled={true}
