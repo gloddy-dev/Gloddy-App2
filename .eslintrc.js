c442fd08720eba285bc7100f04dfe44b3573b84d
Kuesung Park <gueit214@naver.com>
2023-11-13 23:29:27 +0900

    feat : eslint, prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react-native/no-inline-styles': 0,
