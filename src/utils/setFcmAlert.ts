0894ecb899785cb2b1d898550f94e5eb121045ef
Kuesung Park <gueit214@naver.com>
2023-11-28 09:37:55 +0900

    feat : 알림 오면 페이지 push

--- src/utils/setFcmAlert.ts
+++ src/utils/setFcmAlert.ts
@@ -1,22 +1,42 @@
+import { SOURCE_URL } from '@/config';
 import messaging from '@react-native-firebase/messaging';
+import { StackActions } from '@react-navigation/native';
 import { Alert } from 'react-native';
+import { getNotificationPath } from './getNotificationPath';
 
 
 type RemoteMessageType = {
   data: {
+    type: string;
     title: string;
     content: string;
     redirectId: string;
   };
 };
 
-export const setFcmAlert = () => {
+export const setFcmAlert = (navigation) => {
   const unsubscribe = messaging().onMessage((remoteMessage) => {
     const {
       data,
     } = remoteMessage;
-    const { title, content, redirectId } = data as RemoteMessageType['data'];
-    Alert.alert(title, content);
+    const { type, title, content, redirectId } = data as RemoteMessageType['data'];
+
+    const path = getNotificationPath(type, Number(redirectId));
+
+    const pushAction = StackActions.push('WebViewContainer', {
+      url: `${SOURCE_URL}${path}`,
+    });
+
+    Alert.alert(
+      title,
+      content,
+      [
+        {
+          text: 'Go To Page',
+          onPress: () => navigation.dispatch(pushAction),
+        },
+      ],
+    );
   });
 
   messaging().setBackgroundMessageHandler(async remoteMessage => {
