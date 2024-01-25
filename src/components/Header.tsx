2389a1493178c81d267fd20d1c50f47be0a8eec0
Kuesung Park <gueit214@naver.com>
2024-01-25 12:14:16 +0900

    feat : Header 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import theme from '@/styles/theme';
+import {StackHeaderProps} from '@react-navigation/stack';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import Icon from './Icon';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+const Header = ({navigation, route}: StackHeaderProps) => {
+  const {top: topInset} = useSafeAreaInsets();
+  const {title, right} = route.params;
+
+  return (
+    <View style={[styles.wrapper, {paddingTop: topInset}]}>
+      <View style={styles.layout}>
+        <Pressable style={styles.iconButton} onPress={navigation.goBack}>
+          <Icon name="ArrowBack" color={theme.signColors.signPrimary} />
+        </Pressable>
+        {title ? <Text style={styles.title}>{title}</Text> : undefined}
+      </View>
+      <View style={styles.layout}>
+        {right ? (
+          <Pressable style={styles.iconButton} onPress={right}>
+            <Icon name="MoreHorizontal" color={theme.signColors.signPrimary} />
+          </Pressable>
+        ) : undefined}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    backgroundColor: theme.colors.white,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 4,
+  },
+  layout: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconButton: {
+    width: 48,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: theme.signColors.signPrimary,
+    fontSize: 18,
+    fontWeight: '500',
+  },
+});
+
+export default Header;
