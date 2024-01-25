0a66f6d15067204ce57f923cc83596013e14b70f
Kuesung Park <gueit214@naver.com>
2024-01-25 11:37:32 +0900

    chore : 이미지 추가

---
+++ image/index.ts
@@ -0,0 +1,15 @@
+import Union from './union.svg';
+import Matching from './matching.svg';
+import Profile from './profile.svg';
+import ArrowBack from './arrow-back.svg';
+import MoreHorizontal from './more-horizontal.svg';
+
+export const ICON_SVG = {
+  Union,
+  Matching,
+  Profile,
+  ArrowBack,
+  MoreHorizontal,
+};
+
+export type ICON_SVG_KEY = keyof typeof ICON_SVG;
